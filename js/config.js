f9a822e633893214ffa719c1e350464260c3300a
Assumpta Ojukwu <assumptaojukwu.com>
2024-01-02 13:06:58 +0100

    new files3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_oDpPzUmgB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1v1038k1c97hbn45140b2jov3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-central-1_LL8bKUU0c', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '64pnql08ugqngrsaln2f6fbcsl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
